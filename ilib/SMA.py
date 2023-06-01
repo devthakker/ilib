9a4e9d7d6562301d08d57da3d498983c9269fb9b
Devin Thakker <devin.thakker@outlook.com>
2023-06-01 18:05:06 -0500

    add returns

--- ilib/SMA.py
+++ ilib/SMA.py
@@ -25,6 +25,7 @@ class SMA:
         sma = self.data.rolling(self.period).mean()
         self.sma = sma.iloc[self.period-1:]
         self.sma_values = sma
+        return
         
     def add_data_point(self, data_point):
         """
@@ -38,6 +39,7 @@ class SMA:
         self.data = pd.Series(listTemp)
         if len(self.data) > self.period:
             self.calculate()
+        return
                     
     def get_smavalue(self):
         """
@@ -57,6 +59,21 @@ class SMA:
         plt.xlabel
         plt.title('SMA Chart - {} Period'.format(str(self.period)))
         plt.show()
+        return
+        
+    def plot_save(self, filename):
+        """
+        Save the SMA values calculated to a file.
+        """
+        plt.plot(self.sma_values.values, label='SMA')
+        plt.plot(self.data.values, label='Data')
+        plt.legend(loc = 'upper left')
+        plt.ylabel('SMA Values')
+        plt.xlabel
+        plt.title('SMA Chart - {} Period'.format(str(self.period)))
+        plt.savefig(filename)
+        plt.close()
+        return
             
 prices = [44.12, 44.53, 44 , 43.61, 44.33, 44.83, 45.1 , 45.42, 45.84, 46.08, 45.89, 46.03, 45.61, 46.28, 46.28, 46 , 46.03, 46.41, 46.22, 45.64, 46.21, 46.25, 45.71, 46.45, 45.78, 45.35, 44.03, 44.18, 44.22, 44.57, 43.42, 42.66, 43.13]
 sma = SMA(prices, 10)
